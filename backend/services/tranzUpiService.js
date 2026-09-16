82ba31af9bed653dc65b53df7d65729e64052753
Harshit <shakyaharshit683@gmail.com>
2026-09-16 15:15:13 +0530

    feat: complete end-to-end working Buy Now, Cart, UPI QR & Intent payment flow

--- backend/services/tranzUpiService.js
+++ backend/services/tranzUpiService.js
@@ -11,7 +11,7 @@ class TranzUpiService {
     this.merchantId = config.tranzUpi.merchantId || 'DEVCRAFT_MERCHANT';
     this.secret = config.tranzUpi.secret || '';
     this.baseUrl = config.tranzUpi.baseUrl || 'https://api.tranzupi.com';
-    this.merchantVpa = config.tranzUpi.merchantVpa || 'devcraft@upi';
+    this.merchantVpa = config.tranzUpi.merchantVpa || '8630976928@upi';
     this.publicSiteUrl = config.publicSiteUrl || 'https://kiromage.shop';
   }
 
